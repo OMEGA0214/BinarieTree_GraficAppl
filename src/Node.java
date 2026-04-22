603130ef16e08c747cecfe08e099b62dd0d75e0c
OMEGA0214 <davidsantiagoreyes0214@gmail.com>
2026-04-22 15:28:39 -0500

    create of the basic need class, the node and tree

---
+++ src/Node.java
@@ -0,0 +1,37 @@
+
+    public class Node<T extends Comparable<T>> {
+        private T value;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T dato) {
+            this.value = dato;
+            this.left = null;
+            this.right = null;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<T> left) {
+            this.left = left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<T> right) {
+            this.right = right;
+        }
+
+}
---
+++ src/Tree.java
@@ -0,0 +1,5 @@
+public class Tree <T extends Comparable<T>> {
+    private Node<T> root;
+    private int size;
+
+}
